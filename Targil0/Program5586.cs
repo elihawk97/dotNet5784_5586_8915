644acfc3dd5c1b159934f34d44c3209d4459a33c
Eli Hawk <elihawk97@gmail.com>
2023-12-27 23:52:56 +0200

    Initial code + Welcome functions

---
+++ Targil0/Program5586.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Security;
+
+partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome5586();
+    }
+
+    private static void Welcome5586()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+
+    static partial void Welcome8915();
+}
