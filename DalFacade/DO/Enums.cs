6eb4273cd18ef58f422ef93231dfc7d9f2fa321f
soclofa <soclofa@gmail.com>
2024-01-03 21:58:32 +0200

    DO

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DO;
 
-namespace DO
+public record Enums
 {
-    internal class Enums
-    {
-    }
 }
