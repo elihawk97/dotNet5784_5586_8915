aa9fd591161c548c9f94e8944d25db10dd68db80
Eli Hawk <elihawk97@gmail.com>
2024-01-21 00:10:57 +0200

    Added the try catch to create tasks for the dates

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -49,9 +49,6 @@ public static class Initialization
 
             //Setting the Id
             int _id = 123456; // will be set properly in the crud
-/*            do
-                _id = s_rand.Next(TaskNames.Length);
-            while (s_dal!.Task.Read(_id) != null);*/
 
             //setting all the dates
 
@@ -84,22 +81,29 @@ public static class Initialization
             int? EngineerID = null;
 
             //Constructor for Task 
-            Task newTask = new(
-                _id, 
-                Task, 
-                description, 
-                dateCreated, 
-                projectedStartDate, 
-                actualStartTime, 
-                duration, 
-                deadLine, 
-                actualEndDate, 
-                deliverables, 
-                notes, 
-                EngineerID, 
-                experienceLevel);
-
+            try
+            {
+                Task newTask = new(
+                    _id,
+                    Task,
+                    description,
+                    dateCreated,
+                    projectedStartDate,
+                    actualStartTime,
+                    duration,
+                    deadLine,
+                    actualEndDate,
+                    deliverables,
+                    notes,
+                    EngineerID,
+                    experienceLevel);
                 s_dal!.Task.Create(newTask);
+            }
+            catch (InvalidTime ex)
+            {
+                Console.WriteLine(ex);
+            }
+
   
         }
 
