d2622059f930fbe5e7c8facaa0c1fbe3368701a9
Eli Hawk <elihawk97@gmail.com>
2024-01-20 22:40:13 +0200

    Added Serializable to the to the exceptions

--- DalFacade/DO/Exceptions.cs
+++ DalFacade/DO/Exceptions.cs
@@ -1,14 +1,17 @@
-﻿namespace DO;
-
+﻿
+namespace DO;
+[Serializable]
 public class DalDoesNotExistException : Exception
 {
     public DalDoesNotExistException(string? message) : base(message) { }
 }
+[Serializable]
 
 public class DalAlreadyExistsException : Exception
 {
     public DalAlreadyExistsException (string? message) : base(message) { }
 }
+[Serializable]
 
 public class DalDeletionImpossible : Exception
 {
