8c9ee1c77c24950e9dac429f50e31141475d1a11
Eli Hawk <elihawk97@gmail.com>
2024-01-27 21:44:05 +0200

    Added implementation to the Crud functions for using XML

--- DalXml/DependencyImplementation.cs
+++ DalXml/DependencyImplementation.cs
@@ -1,49 +1,117 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//namespace Dal;
 using DO;
 using DalApi;
-namespace DalXml
+using Dal;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Data.SqlTypes;
+using System.Xml;
+namespace DalXml;
+
+internal class DependencyImplementation : IDependency
 {
-    internal class DependencyImplementation : IDependency
+    readonly string s_dependencies_xml = "dependencies";
+
+    public int Create(Dependency entity)
     {
-        readonly string s_dependencies_xml = "dependencies";
+        List<Dependency> dependencies = XMLTools.LoadListFromXMLSerializer<Dependency>(s_dependencies_xml);
+        int _id = XMLTools.GetAndIncreaseNextId(s_dependencies_xml, "Dependency");
+        Dependency copy = entity with { Id = _id };
+        dependencies.Add(copy);
+        XMLTools.SaveListToXMLSerializer<Dependency>(dependencies, s_dependencies_xml);
+        return _id;
+    }
 
-        public int Create(Dependency entity)
-        {
-            throw new NotImplementedException();
-        }
+    public void Delete(int id)
+    {
+        System.Xml.Linq.XElement xElement = XMLTools.LoadListFromXMLElement(s_dependencies_xml);
+        // Find the element with the specified ID and remove it
+        xElement.Descendants("Dependency") // Replace "YourElementName" with the actual name of your XML element
+              .Where(element => (string)element.Attribute("id") == id.ToString())
+              .Remove();
 
-        public void Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
+        // Save the changes back to the XML file
+        XMLTools.SaveListToXMLElement(xElement, s_dependencies_xml);
+    }
 
-        public Dependency? Read(int id)
-        {
-            throw new NotImplementedException();
-        }
+    public Dependency? Read(int id)
+    {
+        System.Xml.Linq.XElement xElement = XMLTools.LoadListFromXMLElement(s_dependencies_xml);
+        // Find the element with the specified ID and remove it
+        XElement foundElement = xElement.Descendants("Dependency") 
+                                      .FirstOrDefault(element => (string)element.Attribute("id") == id.ToString());
 
-        public Dependency? Read(Func<Dependency, bool> filter)
-        {
-            throw new NotImplementedException();
-        }
+        int.TryParse(foundElement.Element("id").Value, out int idValue);
+        int.TryParse(foundElement.Element("dependentTask").Value, out int dependentValue);
+        int.TryParse(foundElement.Element("dependentOnTask").Value, out int dependentOnValue);
 
-        public IEnumerable<Dependency?> ReadAll(Func<Dependency, bool>? filter = null)
-        {
-            throw new NotImplementedException();
-        }
+        return new Dependency(idValue, dependentValue, dependentOnValue);  
+    }
 
-        public void Reset()
-        {
-            throw new NotImplementedException();
-        }
+    public Dependency? Read(Func<Dependency, bool> filter)
+    {
+        System.Xml.Linq.XElement xElement = XMLTools.LoadListFromXMLElement(s_dependencies_xml);
 
-        public void Update(Dependency item)
-        {
-            throw new NotImplementedException();
-        }
+        // Convert XML elements to Dependency objects
+        List<Dependency> dependencies = xElement.Elements("Dependency")
+            .Select(element =>
+            {
+                int.TryParse(element.Element("id").Value, out int idValue);
+                int.TryParse(element.Element("dependentTask").Value, out int dependentValue);
+                int.TryParse(element.Element("dependentOnTask").Value, out int dependentOnValue);
+
+                return new Dependency(idValue, dependentValue, dependentOnValue);
+            })
+            .ToList();
+
+        // Apply the filter to find the matching Dependency
+        Dependency foundDependency = dependencies.FirstOrDefault(filter);
+
+        return foundDependency;
+    }
+
+    public IEnumerable<Dependency?> ReadAll(Func<Dependency, bool>? filter = null)
+    {
+        System.Xml.Linq.XElement xElement = XMLTools.LoadListFromXMLElement(s_dependencies_xml);
+
+        // Convert XML elements to Dependency objects
+        List<Dependency> dependencies = xElement.Elements("Dependency")
+            .Select(element =>
+            {
+                int.TryParse(element.Element("id").Value, out int idValue);
+                int.TryParse(element.Element("dependentTask").Value, out int dependentValue);
+                int.TryParse(element.Element("dependentOnTask").Value, out int dependentOnValue);
+
+                return new Dependency(idValue, dependentValue, dependentOnValue);
+            })
+            .ToList();
+
+
+        return dependencies;
+    }
+
+    public void Reset()
+    {
+
+        // Load the existing XML file into an XDocument
+        XDocument xmlDoc = XDocument.Load(s_dependencies_xml);
+
+        // Clear out all elements from the root
+        xmlDoc.Root?.RemoveAll();
+
+        // Save the modified XDocument back to the same file, overwriting it
+        xmlDoc.Save(s_dependencies_xml);
+    }
+
+    public void Update(Dependency item)
+    {
+        System.Xml.Linq.XElement xElement = XMLTools.LoadListFromXMLElement(s_dependencies_xml);
+        // Find the element with the specified ID and remove it
+        XElement elementToDelete = xElement.Descendants("Dependency") // Replace "YourElementName" with the actual name of your XML element
+                                      .FirstOrDefault(element => (string)element.Attribute("id") == item.Id.ToString());
+        elementToDelete.Remove();
+
+        XMLTools.SaveListToXMLElement(xElement, s_dependencies_xml);
     }
 }
+
