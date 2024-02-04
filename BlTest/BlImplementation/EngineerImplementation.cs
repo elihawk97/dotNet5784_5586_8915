85cb029a2c16a095f82ce1cc4690522aedb2b56c
soclofa <soclofa@gmail.com>
2024-02-04 19:09:04 +0200

    BOEntity Implementation

---
+++ BlTest/BlImplementation/EngineerImplementation.cs
@@ -0,0 +1,32 @@
+﻿using BlApi;
+using BO;
+
+namespace BlImplementation;
+
+internal class EngineerImplementation : IEngineer
+{
+    public void AddEngineer()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteEngineer()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Engineer> ReadAll(Func<Engineer, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Engineer ReadEngineer(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateEngineer(Engineer item)
+    {
+        throw new NotImplementedException();
+    }
+}
