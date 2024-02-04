1560fab17bc14780c44f4e838fe8450d0ae18d4e
soclofa <soclofa@gmail.com>
2024-02-04 19:11:03 +0200

    implementation

--- BlTest/BlImplementation/TaskImplementation.cs
+++ BlTest/BlImplementation/TaskImplementation.cs
@@ -1,7 +1,36 @@
-﻿using BlApi; 
-namespace BlImplementation; 
+﻿using BlApi;
+
+namespace BlImplementation;
 
 internal class TaskImplementation : ITask
 {
+    public void AddTask()
+    {
+        throw new NotImplementedException();
+    }
 
+    public void DeleteTask(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public System.Threading.Tasks.Task Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<System.Threading.Tasks.Task> ReadAll(Func<BO.Task, bool>? filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateStartDate(int id, DateTime newStartTime)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateTask(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
