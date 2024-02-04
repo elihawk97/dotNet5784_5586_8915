b0d24d235c8d1173bef0bb90aa72692635311ad6
Eli Hawk <elihawk97@gmail.com>
2024-02-04 18:05:16 +0200

    Added in ToString and comments for classes in the BO Layer

--- BlTest/BO/Engineer.cs
+++ BlTest/BO/Engineer.cs
@@ -2,6 +2,9 @@
 
 namespace BO; 
 
+/// <summary>
+/// Engineer class for the BO Layer
+/// </summary>
 public class Engineer
 {
     public int Id { get; init; } 
@@ -11,6 +14,19 @@ public class Engineer
 
     public double Cost { get; set; }
     public Task? Task { get; set; }
+
+    /// <summary>
+    /// Over riding the default ToString method to format the data
+    /// </summary>
+    /// <returns>formated object as a string</returns>
+    public override string ToString() => $@"
+    ID={Id}, 
+    Name={Name}, 
+    Email={Email}, 
+    EngineerExperience={Level}, 
+    Cost={Cost}
+    Task={Task}
+    ";
 
 }
 
--- BlTest/BO/Milestone.cs
+++ BlTest/BO/Milestone.cs
@@ -1,11 +1,15 @@
 ﻿using DO;
+using System.Reflection.Emit;
 
 namespace BO; 
 
-
+/// <summary>
+/// Milestone for the BO Layer, defining the completion of 
+/// certain tasks to be a milestone
+/// </summary>
     public class Milestone
     {
-    public int id { get; init; }
+    public int Id { get; init; }
     public string? Name { get; set;}
     public string? Description { get; set; }
     public DateOnly? DateCreated { get; set; }
@@ -17,7 +21,26 @@ namespace BO;
     public string? Comments { get; set; }
     public TaskList? Dependencies { get; set; }
 
-
+    /// <summary>
+    /// Over riding the default ToString method to format the data
+    /// </summary>
+    /// <returns>formated object as a string</returns>
+    public override string ToString()
+    {
+        return $@"
+    ID={Id},
+    Name={Name},
+    Description={Description},
+    Status={Status},
+    Dependencies={(Dependencies != null ? Dependencies.ToString() : "None")},
+    DateCreated={DateCreated},
+    ActualStartDate={ActualStartDate},
+    Deadline={Deadline},
+    ActualEndDate={ActualEndDate},
+    Comments={Comments},
+    Progress={Progress}
+    ";
+    }
 
 
 
