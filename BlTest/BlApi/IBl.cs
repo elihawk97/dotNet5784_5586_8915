b169c60d62e4bf942c2615ca32badc05543af1cb
soclofa <soclofa@gmail.com>
2024-02-04 18:56:40 +0200

    blApi Interfaces

---
+++ BlTest/BlApi/IBl.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace BlApi;
+
+public interface IBl
+{
+    public IEngineer Engineer { get; }
+    public ITask Task { get; }
+    public IMilestone Milestone { get; }
+
+}
--- BlTest/BlApi/IEngineer.cs
+++ BlTest/BlApi/IEngineer.cs
@@ -1,5 +1,18 @@
-﻿namespace BlApi; 
+﻿using DO;
+using System.Data.Common;
+
+namespace BlApi;
+
 public interface IEngineer
 {
+    public IEnumerable<BO.Engineer> ReadAll(Func<BO.Engineer, bool> filter); 
+    public BO.Engineer ReadEngineer(int Id);
+
+    public void AddEngineer();
+
+    public void DeleteEngineer();
+    
+    public void UpdateEngineer(BO.Engineer item);
+
 
 }
