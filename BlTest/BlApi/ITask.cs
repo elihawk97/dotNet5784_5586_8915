e8603b06f30e74baf92e633e8c25ba5105d80dbb
Eli Hawk <elihawk97@gmail.com>
2024-02-04 19:05:18 +0200

    Added the Task interface for the BO layer

--- BlTest/BlApi/ITask.cs
+++ BlTest/BlApi/ITask.cs
@@ -1,4 +1,11 @@
 ﻿namespace BlApi; 
 public interface ITask
 {
+
+    public IEnumerable<Task> ReadAll(Func<BO.Task, bool>? filter);
+    public Task Read(int id);
+    public void AddTask();
+    public void UpdateTask(int id);
+    public void DeleteTask(int id);
+    public void UpdateStartDate(int id, DateTime newStartTime);
 }
